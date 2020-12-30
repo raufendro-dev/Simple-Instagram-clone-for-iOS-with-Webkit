b368a2eaba289cc5a21df6bf0c17e5f63628ec61
raufendro-dev <raufendro@gmail.com>
2020-12-30 22:32:05 +0700

    Upload

--- InstaClone/ViewController.swift
+++ InstaClone/ViewController.swift
@@ -6,12 +6,14 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var webview: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        webview.load(URLRequest(url: URL(string: "https://www.instagram.com")!))
     }
 
 
